1539e11016e041cbbab97163877c4363935203df
MankaranSingh <mankaran32@gmail.com>
2025-04-11 14:21:55 +0530

    new model

--- scripts/v2_rl_walk_mujoco.py
+++ scripts/v2_rl_walk_mujoco.py
@@ -57,7 +57,7 @@ class RLWalk:
         self.policy = OnnxInfer(self.onnx_model_path, awd=True)
 
         self.num_dofs = 14
-        self.max_motor_velocity = 4.8  # rad/s
+        self.max_motor_velocity = 5.5  # rad/s
 
         # Control
         self.control_freq = control_freq
