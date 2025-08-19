9113131ef744cbd1baff5123ee563225dae5b08c
MankaranSingh <mankaran32@gmail.com>
2025-08-19 20:41:09 +0530

    Update eyes.py

--- mini_bdx_runtime/mini_bdx_runtime/eyes.py
+++ mini_bdx_runtime/mini_bdx_runtime/eyes.py
@@ -23,8 +23,7 @@ class Eyes:
         # PWM range for pigpio is 0-255
         self._pwm_range = 255
         
-        self.thread = Thread(target=self.run, daemon=True)
-        self.thread.start()
+        Thread(target=self.run, daemon=True).start()
 
     def set_color(self, r, g, b):
         """Set LED color with floats 0.0–1.0"""
