438b148efe412d04875ab0b7209afb0bc1d6c8ad
MankaranSingh <mankaran32@gmail.com>
2025-11-06 00:05:55 +0530

    Adds multiple retry enabled head_teleop, without ip

--- scripts/v2_rl_walk_mujoco.py
+++ scripts/v2_rl_walk_mujoco.py
@@ -26,7 +26,6 @@ class RLWalk:
         robot="dino",
         initial_policy_type="standing",
         cutoff_freq=50.0,
-        teleop_host="10.144.135.208",
     ):
 
         # Control
@@ -40,8 +39,9 @@ class RLWalk:
         self.eyes = Eyes()
         #self.projector = Projector()
 
-        self.head_teleop_client = HeadTelop(teleop_host)
+        self.head_teleop_client = HeadTelop()
         self.head_teleop_client.start()
+
 
         self.hwi = HWI(serial_port)
         self.imu = Imu(sampling_freq=int(self.control_freq),)
@@ -303,6 +303,7 @@ class RLWalk:
                     self.commands
                 )
 
+                print(self.head_teleop_client.get_rpy_offset())
                 # Apply policy modifier with all the same parameters
                 motor_targets = self.policy_modifier.modify(
                     motor_targets,
@@ -369,12 +370,6 @@ if __name__ == "__main__":
         default=50.0, 
         help="Cutoff frequency for low-pass filter"
     )
-    parser.add_argument(
-        "--teleop_host", 
-        type=str, 
-        default="10.144.135.208", 
-        help="Teleop controller's IP"
-    )
 
     args = parser.parse_args()
     pid = [args.p, args.i, args.d]
@@ -385,7 +380,6 @@ if __name__ == "__main__":
         pid=pid,
         robot=args.robot,
         initial_policy_type=args.policy_type,
-        cutoff_freq=args.cutoff_freq,
-        teleop_host=args.teleop_host
+        cutoff_freq=args.cutoff_freq
     )
     rl_walk.run()
